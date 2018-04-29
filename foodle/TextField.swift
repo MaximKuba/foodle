236110e6be795b0686c14c9404ea1431d273cfe4
MaximKuba <maximkuba@gmail.com>
2018-04-29 16:24:50 +0300

    Init

---
+++ foodle/TextField.swift
@@ -0,0 +1,30 @@
+//
+//  TextField.swift
+//  foodle
+//
+//  Created by Maxim Kuba on 4/29/18.
+//  Copyright © 2018 Maxim Kuba. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TextField: UITextField {
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        let border = CALayer()
+        let width = CGFloat(2.0)
+        border.borderColor = UIColor.darkGray.cgColor
+        border.frame = CGRect(
+            x: 0,
+            y: self.frame.size.height - width,
+            width:  self.frame.size.width,
+            height: self.frame.size.height
+        )
+        
+        border.borderWidth = width
+        self.layer.addSublayer(border)
+        self.layer.masksToBounds = true
+    }
+}
--- foodle/ViewController.swift
+++ foodle/ViewController.swift
@@ -9,7 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: Properti
+    @IBOutlet weak var nameLab: UILabel!
+    @IBOutlet weak var registrationLab: UILabel!
+    @IBOutlet weak var usernameLab: UILabel!
+    @IBOutlet weak var mailLab: UILabel!
+    @IBOutlet weak var passLab: UILabel!
+    @IBOutlet weak var usernameTF: TextField!
+    @IBOutlet weak var mailTF: TextField!
+    @IBOutlet weak var passTF: TextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
